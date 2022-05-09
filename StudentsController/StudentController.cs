be99cc974c2880830720e265a2fdfe9442410dd7
Levi Tracy <LTracy@tql.com>
2022-05-09 16:26:23 -0400

    Add project files.

---
+++ StudentsController/StudentController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace StudentsController
+    {
+    public class StudentController
+        {
+
+        public string ConnectionString { get; set; }
+        public SqlConnection SqlConnection { get; set; }
+
+        public bool ChangeStudent(Student student)
+            {
+            var sql = "UPDATE Student SET"
+                + $" Firstname = '{student.FirstName}'," +
+                $" Lastname = '{student.LastName}', " +
+                $" Statecode = '{student.StateCode}', " +
+                $" SAT = {student.SAT}, " +
+                $" GPA = {student.GPA}," +
+                $" MajorId = {student.MajorId} " +
+                $" where ID = {student.Id};";
+
+            var cmd = new SqlCommand(sql, SqlConnection);
+            var rowsAffected = cmd.ExecuteNonQuery();
+            if (rowsAffected != 1)
+                {
+                throw new Exception("your thingy broke, more than 1  attempted to be added");
+                }
+            return true;
+            }
+
+        public bool AddStudent(Student student)
+            {
+            //check that the connection is established
+            var sql =  "Insert Student " 
+                +" (Firstname, Lastname, Statecode, SAT, GPA, MajorId) "
+                +" Values"
+                +$"('{student.FirstName}', '{student.LastName}','{student.StateCode}', " +
+                $"{student.SAT}, {student.GPA}, {student.MajorId})";
+
+            var cmd  = new SqlCommand(sql, SqlConnection);
+            var rowsAffected = cmd.ExecuteNonQuery();
+            if (rowsAffected != 1)
+                {
+                throw new Exception("your thingy broke, more than 1  attempted to be added");
+                }
+            return true;
+            }
+
+
+        public void OpenConnection()
+            {
+            SqlConnection = new SqlConnection(ConnectionString);
+            SqlConnection.Open();
+            if (SqlConnection.State != System.Data.ConnectionState.Open)
+                {
+                throw new Exception("Connection did not open!");
+                }
+            }
+
+        public void CloseConnection()
+            {
+            SqlConnection.Close();
+            }
+        
+        public StudentController(string ServerInstance, string Database)
+            {
+            ConnectionString = $"server={ServerInstance};"
+                + $"database = {Database};"
+                + "TrustServerCertificate=True;"
+                + "trusted_connection=true;";
+            }
+
+        
+        }
+    }
---
+++ TestStudentsController/Program.cs
@@ -0,0 +1,24 @@
+﻿using StudentsController;
+
+var studctrl = new StudentController("localhost\\sqlexpress", "EdDb");
+
+
+studctrl.OpenConnection();
+
+var student = new Student()
+    {
+    FirstName = "Graham",
+    LastName = "Kraker",
+    StateCode = "AZ",
+    SAT = 3,
+    GPA = 0.1m,
+    MajorId = 1
+    };
+
+student.Id = 69;
+
+var rc = studctrl.ChangeStudent(student);
+
+
+
+studctrl.CloseConnection();
